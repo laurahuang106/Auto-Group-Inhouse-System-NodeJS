bad3902d1e450f024e4fe3c01e9228fdec630fcc
laurahuang106 <huangyuying2018@hotmail.com>
2023-12-17 20:33:28 -0800

    initial commit, create navbar

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs')
+app.use(express.static("public"))
+
+const url = 'MONGODB_ATLAS_CONNECTION_STRING';
+const dbName = 'DATABASE_NAME';
+
+
+app.get('/', async (req, res) => {
+    res.render('index')
+});
+
+app.listen(port)
