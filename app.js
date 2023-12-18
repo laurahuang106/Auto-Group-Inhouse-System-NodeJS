6c3e6cb27144a3206d9a18ede265fa4a5578c122
laurahuang106 <huangyuying2018@hotmail.com>
2023-12-18 14:14:51 -0800

    udpate buyin_report page and send data to db

--- app.js
+++ app.js
@@ -7,10 +7,14 @@ const port = 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
+app.use(express.urlencoded({extended: true}))
 
 const url = `mongodb+srv://laurah:${process.env.DB_PASSWORD}@cluster0.vomr88x.mongodb.net/?retryWrites=true&w=majority`;
 const dbName = 'autoGroup';
 let db;
+
+// placeholder need change to dynamic
+const current_user_id = "65809c3de660c823d34232e9"
 
 // Connect to MongoDB
 MongoClient.connect(url)
@@ -41,6 +45,51 @@ app.get('/inventory', async (req, res) => {
 
 app.get('/buyin_report', async (req, res) => {
     res.render('buyin_report');
+});
+
+app.post('/buyin_report', async (req, res) => {
+    try {
+        // set default reconditioning cost
+        const reconditioning_cost = 0
+
+        // placeholder, hardcoded user and branch 
+        const user_id = "65809c3de660c823d34232e9"
+        const branch_location = "Lafayette, IN"
+
+        // Extract form data
+        const formData = req.body;
+        const vehicleData = {
+            VIN: formData.vin,
+            make: formData.make,
+            model: formData.model,
+            year_of_production: formData.year,
+            mileage: formData.mileage,
+            fuel_type: formData.fuel_type,
+            branch_location: branch_location,
+            buyin_date: formData.buyin_date,
+            buyin_price: formData.buyin_price,
+            reconditioning_cost: reconditioning_cost,
+        };
+
+        // Insert vehicle data into the 'vehicles' collection
+        const vehicleResult = await db.collection('vehicles').insertOne(vehicleData);
+        const vehicleId = vehicleResult.insertedId;
+
+        // Prepare buyin record data
+        const buyinRecordData = {
+            buyin_date: formData.buyin_date,
+            vehicle_id: vehicleId,
+            sales_person_id: user_id, 
+            buyin_price: formData.buyin_price
+        };
+
+        // Insert buyin record into the 'buyin_records' collection
+        await db.collection('buyin_records').insertOne(buyinRecordData);
+
+        res.redirect('/inventory'); 
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
 });
 
 app.get('/profile', async (req, res) => {
