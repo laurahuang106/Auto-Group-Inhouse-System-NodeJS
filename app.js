86276e4b0ba244474671c9fbda17129c8bbaecb6
laurahuang106 <huangyuying2018@hotmail.com>
2023-12-18 12:08:33 -0800

    connect to mongodb

--- app.js
+++ app.js
@@ -1,33 +1,48 @@
+require('dotenv').config()
 const express = require('express');
+const { MongoClient } = require('mongodb');
 
 const app = express();
 const port = 3000;
 
-app.set('view engine', 'ejs')
-app.use(express.static("public"))
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
 
-const url = 'MONGODB_ATLAS_CONNECTION_STRING';
-const dbName = 'DATABASE_NAME';
+const url = `mongodb+srv://laurah:${process.env.DB_PASSWORD}@cluster0.vomr88x.mongodb.net/?retryWrites=true&w=majority`;
+const dbName = 'autoGroup';
+let db;
 
+// Connect to MongoDB
+MongoClient.connect(url)
+    .then(client => {
+        db = client.db(dbName);
+        app.listen(port, () => console.log(`Server running on port ${port}`));
+    })
+    .catch(err => {
+        console.error("Failed to connect to MongoDB", err);
+    });
 
 app.get('/', async (req, res) => {
-    res.render('index')
+    res.render('index');
 });
 
 app.get('/orders', async (req, res) => {
-    res.render('sale_orders')
+    res.render('sale_orders');
 });
 
 app.get('/inventory', async (req, res) => {
-    res.render('inventory')
+    try {
+        const vehicles = await db.collection('vehicles').find({}).toArray();
+        res.render('inventory', { vehicles: vehicles }); 
+    } catch (error) {
+        res.status(500).send(error);
+    }
 });
 
 app.get('/buyin_report', async (req, res) => {
-    res.render('buyin_report')
+    res.render('buyin_report');
 });
 
 app.get('/profile', async (req, res) => {
-    res.render('profile')
+    res.render('profile');
 });
-
-app.listen(port)
