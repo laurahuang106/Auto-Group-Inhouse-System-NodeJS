c30db532998f7a4a4bb0ce535aa28386b383e6b5
laurahuang106 <huangyuying2018@hotmail.com>
2023-12-20 13:58:41 -0800

    udpate orders page by applying filters

--- app.js
+++ app.js
@@ -30,6 +30,90 @@ app.get('/', async (req, res) => {
 
 app.get('/orders', async (req, res) => {
     try {
+        // fetch filters data 
+        const branches = await db.collection('users').distinct('branch');
+        const sale_dates = await db.collection('sale_orders').distinct('sale_date');
+        const makes = await db.collection('vehicles').distinct('make');
+        const models = await db.collection('vehicles').distinct('model');
+        const sales_person_fullnames = await db.collection('users').aggregate([
+            {
+                $project: {
+                    fullname: { $concat: ["$f_name", " ", "$l_name"] }
+                }
+            }
+        ]).toArray();
+        /// find the minimum and maximum year of production
+        const yearRangeResult = await db.collection('vehicles').aggregate([
+            {
+                $group: {
+                    _id: null,
+                    min_year: { $min: "$year_of_production" },
+                    max_year: { $max: "$year_of_production" }
+                }
+            }
+        ]).toArray();
+        const minYear = yearRangeResult[0] ? yearRangeResult[0].min_year : null;
+        const maxYear = yearRangeResult[0] ? yearRangeResult[0].max_year : null;
+        const year_gap = 1; 
+        const years = [];
+        for (let i = minYear; i <= maxYear; i += year_gap) {
+            years.push(i);
+        }
+        // hardcoded max mileage of all cars
+        const max_mileage = 100000
+        const mileage_gap = 10000; 
+        const mileages = [];
+        for (let i = 0; i <= max_mileage; i += mileage_gap) {
+            mileages.push(i);
+        }
+
+
+        // add user options to filterCritiria to filter table
+        let filterCriteria = {};
+
+        if (req.query.start_date) {
+            filterCriteria['sale_date'] = { $gte: new Date(req.query.start_date) };
+        }
+        if (req.query.end_date) {
+            filterCriteria['sale_date'] = { ...filterCriteria['sale_date'], $lte: new Date(req.query.end_date) };
+        }
+
+        if (req.query.employee_dropdown && req.query.employee_dropdown !== '') {
+            const [f_name, ...l_name] = req.query.employee_dropdown.split(" ");
+            filterCriteria['sales_person_info.f_name'] = f_name;
+            filterCriteria['sales_person_info.l_name'] = l_name.join(" "); 
+        }
+
+        if (req.query.branch_dropdown && req.query.branch_dropdown !== '') {
+            filterCriteria['sales_person_info.branch'] = req.query.branch_dropdown;
+        }
+
+        if (req.query.make_dropdown && req.query.make_dropdown !== '') {
+            filterCriteria['vehicle_info.make'] = req.query.make_dropdown;
+        }
+
+        if (req.query.model_dropdown && req.query.model_dropdown !== '') {
+            filterCriteria['vehicle_info.model'] = req.query.model_dropdown;
+        }
+
+        if (req.query.min_year_dropdown && req.query.min_year_dropdown !== '') {
+            filterCriteria['vehicle_info.year_of_production'] = { $gte: parseInt(req.query.min_year_dropdown) };
+        }
+        if (req.query.max_year_dropdown && req.query.max_year_dropdown !== '') {
+            filterCriteria['vehicle_info.year_of_production'] = { $lte: parseInt(req.query.max_year_dropdown) };
+        }
+
+        if (req.query.min_mile_dropdown && req.query.min_mile_dropdown !== '') {
+            filterCriteria['vehicle_info.mileage'] = { $gte: parseInt(req.query.min_mile_dropdown) };
+        }
+        if (req.query.max_mile_dropdown && req.query.max_mile_dropdown !== '') {
+            filterCriteria['vehicle_info.mileage'] = { $lte: parseInt(req.query.max_mile_dropdown) };
+        }
+
+        const sortCriteria = { order_date: -1, make: -1, model: -1, year: -1, mileage: -1, sale_price: -1, profit: -1} 
+
+
+        // fetch data for table
         const orders = await db.collection('sale_orders').aggregate([
             {
                 $lookup: {
@@ -54,6 +138,9 @@ app.get('/orders', async (req, res) => {
                 $unwind: "$sales_person_info"
             },
             {
+                $match: filterCriteria
+            },
+            {
                 $project: {
                     order_id: "$_id",
                     make: "$vehicle_info.make",
@@ -66,10 +153,23 @@ app.get('/orders', async (req, res) => {
                     sales_person_name: { $concat: ["$sales_person_info.f_name", " ", "$sales_person_info.l_name"] },
                     store_branch: "$sales_person_info.branch"
                 }
+            },
+            {
+                $sort: sortCriteria
             }
         ]).toArray();
 
-        res.render('sale_orders', { orders });
+        res.render('sale_orders', { 
+            orders,
+            sale_dates,
+            makes,
+            models,
+            years,
+            mileages,
+            branches,
+            sales_person_fullnames,
+            query: req.query 
+        });
     } catch (error) {
         res.status(500).send(error.message);
     }
